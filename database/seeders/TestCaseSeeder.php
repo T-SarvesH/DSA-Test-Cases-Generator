748e2acc2fe827ff779b1025639acc089e84a976
Shaun Menezes <shaun.menezes1724@gmail.com>
2025-03-23 20:17:26 +0530

    Added entries to seeder

--- database/seeders/TestCaseSeeder.php
+++ database/seeders/TestCaseSeeder.php
@@ -26,5 +26,76 @@ class TestCaseSeeder extends Seeder
             'input' => 'hello',
             'expected_output' => 'olleh'
         ]);
+
+        TestCase::create([
+            'problem_title' => 'Sum of Two Numbers',
+            'description' => 'Given two numbers, find their sum.',
+            'input' => '4 7',
+            'expected_output' => '11'
+        ]);
+
+        TestCase::create([
+            'problem_title' => 'Check Even or Odd',
+            'description' => 'Given a number, determine if it is even or odd.',
+            'input' => '6',
+            'expected_output' => 'even'
+        ]);
+
+        TestCase::create([
+            'problem_title' => 'Count Vowels',
+            'description' => 'Given a string, count the number of vowels.',
+            'input' => 'beautiful',
+            'expected_output' => '5'
+        ]);
+
+        TestCase::create([
+            'problem_title' => 'Find Factorial',
+            'description' => 'Given a number, find its factorial.',
+            'input' => '5',
+            'expected_output' => '120'
+        ]);
+
+        TestCase::create([
+            'problem_title' => 'Check Palindrome',
+            'description' => 'Given a string, check if it is a palindrome.',
+            'input' => 'madam',
+            'expected_output' => 'true'
+        ]);
+
+        TestCase::create([
+            'problem_title' => 'Find Second Largest Number',
+            'description' => 'Given an array of numbers, find the second largest number.',
+            'input' => '10 20 4 45 99',
+            'expected_output' => '45'
+        ]);
+
+        TestCase::create([
+            'problem_title' => 'Convert Celsius to Fahrenheit',
+            'description' => 'Given a temperature in Celsius, convert it to Fahrenheit.',
+            'input' => '25',
+            'expected_output' => '77'
+        ]);
+
+        TestCase::create([
+            'problem_title' => 'Find GCD of Two Numbers',
+            'description' => 'Given two numbers, find their greatest common divisor (GCD).',
+            'input' => '36 48',
+            'expected_output' => '12'
+        ]);
+
+        TestCase::create([
+            'problem_title' => 'Check Prime Number',
+            'description' => 'Given a number, determine if it is prime.',
+            'input' => '29',
+            'expected_output' => 'true'
+        ]);
+
+        TestCase::create([
+            'problem_title' => 'Fibonacci Sequence',
+            'description' => 'Given a number n, return the first n terms of the Fibonacci sequence.',
+            'input' => '5',
+            'expected_output' => '0 1 1 2 3'
+        ]);
+
     }
 }
